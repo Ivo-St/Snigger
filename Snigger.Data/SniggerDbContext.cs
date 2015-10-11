a308d225689d8473d7ecc8b120590cbfb1a63c5e
Ivailo Stoianov <ivostoianov94@gmail.com>
2015-10-11 21:18:46 +0300

    Create Notifications, Users and Snickers controllers. Move users model to the models project. Configure the app to use the db context

--- Snigger.Data/SniggerDbContext.cs
+++ Snigger.Data/SniggerDbContext.cs
@@ -1,18 +1,23 @@
 namespace Snigger.Data
 {
+    using Microsoft.AspNet.Identity.EntityFramework;
     using Models;
     using System;
     using System.Data.Entity;
     using System.Linq;
 
-    public class SniggerDbContext : DbContext
+    public class SniggerDbContext : IdentityDbContext<User>
     {
         public SniggerDbContext()
             : base("name=SniggerDbContext")
         {
         }
 
-        public IDbSet<User> Users { get; set; }
+        public static SniggerDbContext Create()
+        {
+            return new SniggerDbContext();
+        }
+
         public IDbSet<Snicker> Snickers { get; set; }
 
     }
---
+++ Snigger.Web/Controllers/SnickersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Snigger.Web.Controllers
+{
+    public class SnickersController : Controller
+    {
+        // GET: Snickers
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
